8fb1a111523dc268c9a6e821d3e6aee4807fd386
Schyrsivochter <andreas.kuebrich@kuebrich.de>
2015-11-01 15:05:53 +0100

    Deleted comp.py
    It’s a helper script no longer needed.

--- comp.py
+++
@@ -1,6 +0,0 @@
-import py_compile as pyc, os, sys
-
-scaPath = os.path.dirname(__file__)
-
-pyc.compile(scaPath + "\\sca.py", scaPath + "\\WD\\sca.pyc")
-pyc.compile(scaPath + "\\scaguioo.py", scaPath + "\\WD\\scaguioo.pyc")
